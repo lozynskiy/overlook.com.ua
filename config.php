84e409811da05f9aac5895b5e05466920bd193f1
Lozynskiy <loz.misha@gmail.com>
2017-08-06 22:39:49 +0300

    config file

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://overlook/');
+define('HTTP_SERVER', 'http://overlook.com.ua/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://overlook/');
+define('HTTPS_SERVER', 'http://overlook.com.ua/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/OS/OpenServer/domains/overlook/catalog/');
-define('DIR_SYSTEM', 'C:/OS/OpenServer/domains/overlook/system/');
-define('DIR_IMAGE', 'C:/OS/OpenServer/domains/overlook/image/');
-define('DIR_LANGUAGE', 'C:/OS/OpenServer/domains/overlook/catalog/language/');
-define('DIR_TEMPLATE', 'C:/OS/OpenServer/domains/overlook/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/OS/OpenServer/domains/overlook/system/config/');
-define('DIR_CACHE', 'C:/OS/OpenServer/domains/overlook/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/OS/OpenServer/domains/overlook/system/storage/download/');
-define('DIR_LOGS', 'C:/OS/OpenServer/domains/overlook/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/OS/OpenServer/domains/overlook/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/OS/OpenServer/domains/overlook/system/storage/upload/');
+define('DIR_APPLICATION', '/home/overlook/public_html/catalog/');
+define('DIR_SYSTEM', '/home/overlook/public_html/system/');
+define('DIR_IMAGE', '/home/overlook/public_html/image/');
+define('DIR_LANGUAGE', '/home/overlook/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/overlook/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/overlook/public_html/system/config/');
+define('DIR_CACHE', '/home/overlook/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/overlook/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/overlook/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/overlook/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/overlook/public_html/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'admin');
-define('DB_DATABASE', 'admin');
+define('DB_USERNAME', 'overlook_admin');
+define('DB_PASSWORD', 'overlook1508!');
+define('DB_DATABASE', 'overlook_db');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
