7682ed6b6d44cc767e642221b6ce5e4daf1aec3f
Lozynskiy <loz.misha@gmail.com>
2017-09-11 07:53:39 +0300

    add privat24 payment

--- admin/config.php
+++ admin/config.php
@@ -13,18 +13,18 @@ define('HTTPS_CATALOG', 'http://'.$host.'/');
 $dir = dirname(dirname(__FILE__));
 
 // DIR
-define('DIR_APPLICATION', $dir.'/admin/');
-define('DIR_SYSTEM', $dir.'/system/');
-define('DIR_IMAGE', $dir.'/image/');
-define('DIR_LANGUAGE', $dir.'/admin/language/');
-define('DIR_TEMPLATE', $dir.'/admin/view/template/');
-define('DIR_CONFIG', $dir.'/system/config/');
-define('DIR_CACHE', $dir.'/system/storage/cache/');
-define('DIR_DOWNLOAD', $dir.'/system/storage/download/');
-define('DIR_LOGS', $dir.'/system/storage/logs/');
-define('DIR_MODIFICATION', $dir.'/system/storage/modification/');
-define('DIR_UPLOAD', $dir.'/system/storage/upload/');
-define('DIR_CATALOG', $dir.'/catalog/');
+define('DIR_APPLICATION', 'C:/OS/OpenServer/domains/overlook/admin/');
+define('DIR_SYSTEM', 'C:/OS/OpenServer/domains/overlook/system/');
+define('DIR_IMAGE', 'C:/OS/OpenServer/domains/overlook/image/');
+define('DIR_LANGUAGE', 'C:/OS/OpenServer/domains/overlook/admin/language/');
+define('DIR_TEMPLATE', 'C:/OS/OpenServer/domains/overlook/admin/view/template/');
+define('DIR_CONFIG', 'C:/OS/OpenServer/domains/overlook/system/config/');
+define('DIR_CACHE', 'C:/OS/OpenServer/domains/overlook/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OS/OpenServer/domains/overlook/system/storage/download/');
+define('DIR_LOGS', 'C:/OS/OpenServer/domains/overlook/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OS/OpenServer/domains/overlook/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OS/OpenServer/domains/overlook/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OS/OpenServer/domains/overlook/catalog/');
 
 // DB
 
