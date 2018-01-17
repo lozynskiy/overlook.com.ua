f5ecf121d7104e7965b7e0f2ae4a5bc31a6ba5b0
loz.misha@gmail.com <HDradeon5650!>
2018-01-17 18:55:04 +0200

    category page size

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -330,7 +330,7 @@ class ControllerProductCategory extends Controller {
 
 			$data['limits'] = array();
 
-			$limits = array_unique(array($this->config->get($this->config->get('config_theme') . '_product_limit'), 25, 50, 75, 100));
+			$limits = array_unique(array($this->config->get($this->config->get('config_theme') . '_product_limit'), 24, 52, 72, 100));
 
 			sort($limits);
 
